9f1df211daeeab9467eb17d791f64ce3b7ff4694
Matt Arnold <matt.arnd@gmail.com>
2022-08-30 16:14:12 -0500

    undid last changes

--- LFGHub/Controllers/PostController.cs
+++ LFGHub/Controllers/PostController.cs
@@ -24,7 +24,6 @@ public class PostController : Controller
         }
     }
 
-    // d
     // db is just a variable name, can be called anything (e.g. DATABASE, db, _db, etc)
     private DatabaseContext _context;
      
